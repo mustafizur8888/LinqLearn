f6be1bcb9941f97b39f787bea4a295503a6a416b
Mustafizur Rahman <mustafizuzr8888@gmail.com>
2016-12-25 11:42:36 +0600

    Day 2 commit

--- D1E2/Program.cs
+++ D1E2/Program.cs
@@ -1,44 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace D1E2
 {
     class Program
     {
         static void Main(string[] args)
+        //var students = from x in Student.GetStudentList()
+        //               where x.Age>30
         {
-            List<Student> students = new List<Student>()
-            {
-                new Student()
-                {
-                    ID = 1,
-                    Name = "Rasel",
-                    Age = 20
-                },
-                new Student()
-                {
-                    ID = 2,
-                    Name = "Mustafiz", 
-                    Age = 23
-                },
-                new Student()
-                {
-                    ID = 3,
-                    Name = "NK",
-                    Age = 45
-                },
-                new Student()
-                {
-                    ID = 4,
-                    Name = "TH",
-                    Age = 50
-                },
-                new Student()
-                {
-                    ID = 5,
-                    Name = "HP",
-                    Age = 35
-                }
-            };
+            //sql like syntax
+            //               select x;
+
+
+            // lamda syntax
+            // var students = Student.GetStudentList().OrderBy(x => x.Age);
+            //var students = new Student().GetStudentList().Where(x => x.Age > 30).Select(x => new
+            //{
+            //    AlisName = x.Name,
+            //    Id = x.Id
+            //});
+
+            var a = new[] { 1, 5, 7, 3, 9 };
+            var s = new[] { "a", "b", "c", "d" };
+
+            var sList = new Student().GetStudentList();
+
+            Student firstOrDefault = sList.FirstOrDefault();
+
+            if (firstOrDefault != null)
+                Console.WriteLine("Name " + firstOrDefault.Name);
+            else
+                Console.WriteLine("Student Not Found");
+
+
+            Console.WriteLine("a :" + a.FirstOrDefault(aa => aa % 2 == 0));
+
+            Console.WriteLine("s : " + s.FirstOrDefault(x => x.Contains("e")));
+
 
 
 
@@ -48,8 +48,60 @@ namespace D1E2
 
     class Student
     {
-        public int ID { get; set; }
+        private readonly List<Student> _students;
+
+        public Student()
+        {
+            _students = new List<Student>();
+        }
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+
+        public List<Student> GetStudentList()
+        {
+            _students.Add(new Student()
+            {
+                Id = 1,
+                Name = "Rasel",
+                Age = 20
+            });
+            _students.Add(new Student()
+            {
+                Id = 2,
+                Name = "Mustafiz",
+                Age = 23
+            });
+
+            _students.Add(new Student()
+            {
+                Id = 3,
+                Name = "NK",
+                Age = 45
+            });
+            _students.Add(
+                new Student()
+                {
+                    Id = 4,
+                    Name = "TH",
+                    Age = 50
+                });
+            _students.Add(
+                new Student()
+                {
+                    Id = 5,
+                    Name = "HP",
+                    Age = 35
+                }
+                );
+
+
+            return _students;
+        }
+
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
     }
 }
--- D1E2/Properties/AssemblyInfo.cs
+++ D1E2/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The following GUID is for the Id of the typelib if this project is exposed to COM
 [assembly: Guid("61199c42-9c6e-4108-925f-7ffd53de272e")]
 
 // Version information for an assembly consists of the following four values:
